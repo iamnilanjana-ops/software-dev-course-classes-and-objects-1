e2b742b679c99560058ff89b84cdfa09aea92d57
iamnilanjana-ops <iamnilanjana@gmail.com>
2026-05-11 12:33:33 -0500

    Completed unit testing practice

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,23 +6,22 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter the first number: ");
         int a = scanner.nextInt();
+
         System.out.print("Enter the second number: ");
         int b = scanner.nextInt();
 
-        // Use the static methods you wrote in the Calculator class
-        // to assign proper values to the four variables below
-        // (instead of 0)
-        int sum = 0;
-        int difference = 0;
-        int product = 0;
-        int quotient = 0;
+        // Use methods from Calculator class
+        int sum = Calculator.add(a, b);
+        int difference = Calculator.subtract(a, b);
+        int product = Calculator.multiply(a, b);
+        int quotient = Calculator.divide(a, b);
 
         System.out.println("The sum is " + sum);
         System.out.println("The difference is " + difference);
         System.out.println("The product is " + product);
         System.out.println("The quotient is " + quotient);
     }
-
 }
